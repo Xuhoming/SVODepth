f0a837ff94b4f921ca865402607874a313a1fcef
Connie Wu <cwu@padl145.magicleap.ds>
2016-07-27 15:24:13 -0700

    add submodule8

---
+++ catkin_ws/src/rpg_svo/svo/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cwu/Experiment/SVO/catkin_ws/src/rpg_svo/svo/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cwu/Experiment/SVO/catkin_ws/src/rpg_svo/svo/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/cwu/Experiment/SVO/catkin_ws/src/rpg_svo/svo:$ROS_PACKAGE_PATH"
