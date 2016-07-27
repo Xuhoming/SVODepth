79e74268344727d7f728f0811e3226187a1fc2db
Connie Wu <cwu@padl145.magicleap.ds>
2016-07-27 15:04:04 -0700

    add submodule2

---
+++ workspace/Sophus/build/CMakeFiles/Sophus.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cwu/Experiment/SVO/workspace/Sophus/sophus/scso3.cpp" "/home/cwu/Experiment/SVO/workspace/Sophus/build/CMakeFiles/Sophus.dir/sophus/scso3.cpp.o"
+  "/home/cwu/Experiment/SVO/workspace/Sophus/sophus/se2.cpp" "/home/cwu/Experiment/SVO/workspace/Sophus/build/CMakeFiles/Sophus.dir/sophus/se2.cpp.o"
+  "/home/cwu/Experiment/SVO/workspace/Sophus/sophus/se3.cpp" "/home/cwu/Experiment/SVO/workspace/Sophus/build/CMakeFiles/Sophus.dir/sophus/se3.cpp.o"
+  "/home/cwu/Experiment/SVO/workspace/Sophus/sophus/sim3.cpp" "/home/cwu/Experiment/SVO/workspace/Sophus/build/CMakeFiles/Sophus.dir/sophus/sim3.cpp.o"
+  "/home/cwu/Experiment/SVO/workspace/Sophus/sophus/so2.cpp" "/home/cwu/Experiment/SVO/workspace/Sophus/build/CMakeFiles/Sophus.dir/sophus/so2.cpp.o"
+  "/home/cwu/Experiment/SVO/workspace/Sophus/sophus/so3.cpp" "/home/cwu/Experiment/SVO/workspace/Sophus/build/CMakeFiles/Sophus.dir/sophus/so3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ workspace/Sophus/build/SophusConfig.cmake
@@ -0,0 +1,17 @@
+################################################################################
+# Sophus source dir
+set( Sophus_SOURCE_DIR "/home/cwu/Experiment/SVO/workspace/Sophus")
+
+################################################################################
+# Sophus build dir
+set( Sophus_DIR "/home/cwu/Experiment/SVO/workspace/Sophus/build")
+
+################################################################################
+set( Sophus_INCLUDE_DIR  "/home/cwu/Experiment/SVO/workspace/Sophus;/usr/include/eigen3" )
+set( Sophus_INCLUDE_DIRS  "/home/cwu/Experiment/SVO/workspace/Sophus;/usr/include/eigen3" )
+
+set( Sophus_LIBRARIES    "/home/cwu/Experiment/SVO/workspace/Sophus/build/libSophus.so" )
+set( Sophus_LIBRARY      "/home/cwu/Experiment/SVO/workspace/Sophus/build/libSophus.so" )
+
+set( Sophus_LIBRARY_DIR  "/home/cwu/Experiment/SVO/workspace/Sophus/build" )
+set( Sophus_LIBRARY_DIRS "/home/cwu/Experiment/SVO/workspace/Sophus/build" )
